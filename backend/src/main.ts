aee42664b7df8084d1b8505d37deabe29b2347f3
faizscripts <4faizahmed@gmail.com>
2025-10-29 17:12:55 +0300

    chore(cors): enable dynamic cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
 
@@ -12,6 +14,19 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const corsOrigins = configService.get<string>('CORS_ORIGIN');
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || corsOrigins) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS blocked: ${origin}`));
+      }
+    },
+    credentials: true,
+  });
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
