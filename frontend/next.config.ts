758ef49edb3ae07f5e799b2ffa6d6afefb895840
faizscripts <4faizahmed@gmail.com>
2026-01-07 10:12:31 +0300

    fix(frontend): update BE deployment path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/proxy/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_DESTINATION_URL}/api/v1/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
